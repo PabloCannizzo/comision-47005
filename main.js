7f21e06d11b9ca63ccbcaaa2caf6f8cfbbb5752c
pablo cannizzo <pablocannizzo@gmail.com>
2023-09-04 21:35:20 -0300

    3era pre-entrega

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ h1.innerHTML = "LiquorStore";
 parrafo.innerHTML = "tienda de bebidas online";
 h1.className = "verde";
 parrafo.className = "subtitulo";
-header.append(h1, parrafo); // bien
+header.append(h1, parrafo); 
 
 alert("Bienvenido a LiquorStore");
 //alert("A continuación le pediremos unos datos, con el fin de brindarle una mejor atención, Gracias!"); 
@@ -60,159 +60,23 @@ let direccion = prompt("Ingrese su direccion de residencia");
 const persona1 = new Persona(nombre, edad, direccion);
 
 sessionStorage.setItem("persona1", JSON.stringify(persona1));
+
+//Agregar mas funciones del storage y json
+/* let usuario;
+let usuarioStorage = sessionStorage.getItem("persona1");
+if (usuarioStorage =! ""){
+    usuario = usuarioStorage;
+    alert(`Bienvenido nuevamente ${persona1.nombre}`);
+}
+else{
+    usuario = prompt("ingrese su nombre");
+    sessionStorage.setItem("usuario", usuario);
+    alert(`Bienvenido por primera vez a LiquorStore`);
+} */
 /* sessionStorage.setItem("nombre", persona1.nombre);
 sessionStorage.setItem("edad", persona1.edad);
 sessionStorage.setItem("direccion", persona1.direccion); */
 
-/* let div1 = document.createElement("div");
-div1.className = "categorias";
-div1.innerHTML = `<h2>Bienvenido ${persona1.nombre} a nuestra tienda de bebidas online</h2>
-<h3>A continuación ingrese la categoria de bebida que desea comprar...</h3>`;
-cuerpo.append(div1); */
-/* 
-if ((persona1.nombre != "") && (persona1.edad >= 18)) {
-    alert(`Bienvenido ${persona1.nombre} a nuestra tienda online de bebidas.`);
-    alert("A continuación ingrese la categoria de bebida que desea comprar");
-    while (nombre.toLowerCase() !== "esc") {
-        if (persona1.edad >= 18) {
-            let opciones = "Opciones de bebidas con y sin alcohol:\n";
-            productos.forEach((producto, index) => {
-                opciones += `${index + 1}. ${producto.nombre}\n`;
-            });
-            opciones += "Elija una opción escribiendo el número correspondiente:";
-
-            const opcion = parseInt(prompt(opciones));
-
-            if (opcion >= 1 && opcion <= productos.length) {
-                const producto = productos[opcion - 1];
-                let mensaje = `
-                Usted eligio: ${producto.nombre}
-                Categoria: ${producto.categoria}
-                Precio c/u: $ ${producto.precio}
-                Stock disponible: ${producto.cantidad} unidades`;
-                alert(mensaje);
-                let cantidad = parseInt(prompt("Ingrese la cantidad a comprar: #..."));
-                let cantidadStock1 = resta(producto.cantidad, cantidad);
-                let precioF = resta(suma(mult(cantidad, producto.precio), iva(producto.precio)), producto.descuento);
-                alert(`El valor de cada botella es de $${producto.precio}. Su valor final de compra + impuestos es igual= $${precioF}. Cantidad actual del stock: ${cantidadStock1} unidades.`);
-            }
-            else {
-                alert("La categoria que eligio no se encuentra por el momento. Elija otra variedad de bebida, gracias!");
-            }
-        }
-        nombre = prompt("Si desea consultar un nuevo producto ingreselo a continuación. Caso contrario escriba 'esc' para salir");
-    }
-    let envio = prompt("Desea elegir nuestro sistema de envio a domicilio?. A continuación seleccione SI o NO");
-    if ((envio === "si") || (envio === "SI")) {
-        alert(`Confirme sus datos ingresados: 
-        Nombre: ${persona1.nombre}
-        Edad: ${persona1.edad}
-        Dirección: ${persona1.direccion}`);
-        let confirmacion = prompt("Si son correctos escriba SI, caso contrario escriba MODIFICAR/NO");
-        switch (confirmacion) {
-            case "si":
-                alert("Excelente, su pedido sera preparado y enviado por nuestro cadete");
-                break;
-            case "SI":
-                alert("Excelente, su pedido sera preparado y enviado por nuestro cadete");
-                break;
-            default:
-                let nombre = prompt("Ingrese nuevamente su nombre");
-                let edad = parseInt(prompt("Ingrese nuevamente su edad"));
-                let direccion = prompt("Ingrese nuevamente su direccion de residencia");
-                const persona2 = new Persona(nombre, edad, direccion);
-                while (nombre != "ESC") {
-                    alert(`Confirme sus datos:
-                    Nombre: ${persona2.nombre}
-                    Edad: ${persona2.edad}
-                    Direccion: ${persona2.direccion}`);
-                    nombre = prompt("Si sus datos son correctos escriba 'ESC' para continuar. Caso contrario debe volver a ingresar sus datos ")
-                }//ver como agregar informacion para el envio
-                alert("Excelente, su pedido sera preparado y enviado por nuestro cadete");
-                break;
-        }
-    }
-    else {
-        alert("Excelente, su pedido sera preparado para su posterior retiro por la sucursal. Recuerde que nuestro horario de atención es de Lunes a Viernes de 9:30 hs a 22 hs y días Sabados de 9:30 hs a 14 hs.")
-    }
-    alert("MUCHAS GRACIAS POR ELEGIRNOS...");
-}
-else if ((persona1.nombre != "") && (18 > persona1.edad)) {
-    alert(`Bienvenido ${persona1.nombre} a nuestra tienda online de bebidas. La venta de alcohol a menores de 18 años esta PROHIBIDA. Disculpe las molestias!`);
-    alert("Contamos con una amplia variedad de bebidas sin alcohol dentro nuestro stock: Gaseosas, Jugos, aguas, entre otras. Seleccione alguna de las opcione a continuación:..."); // Para menores de 18 años contamos con 2 
-
-    const productosSinAlcohol = productos.filter(producto => producto.categoria === "sin alcohol");
-    while (nombre.toLowerCase() !== "esc") {
-        if (persona1.edad < 18) {
-            let opcionesSinAlcohol = "Opciones de bebidas sin alcohol:\n";
-            productosSinAlcohol.forEach((producto, index) => {
-                opcionesSinAlcohol += `${index + 1}. ${producto.nombre}\n`;
-            });
-            opcionesSinAlcohol += "Elija una opción escribiendo el número correspondiente:";
-
-            const opcion = parseInt(prompt(opcionesSinAlcohol));
-
-            if (opcion >= 1 && opcion <= productosSinAlcohol.length) {
-                const producto = productosSinAlcohol[opcion - 1];
-                alert(`
-                Usted eligio: ${producto.nombre}
-                categoria: ${producto.categoria}
-                Precio c/u: $ ${producto.precio}
-                Stock Disponible: ${producto.cantidad} unidades `);
-                let cantidad = parseInt(prompt("Ingrese la cantidad a comprar: #..."));
-                let cantidadStock1 = resta(producto.cantidad, cantidad);
-                let precioF = resta(suma(mult(cantidad, producto.precio), iva(producto.precio)), producto.descuento);
-
-                alert(`El valor de cada botella es de $${producto.precio}. Su valor final de compra + impuestos es igual= $${precioF}. Cantidad actual del stock: ${cantidadStock1} unidades.`);
-                // Resto del código para mostrar información y procesar la compra
-
-            }
-            else {
-                alert("Opción inválida. Elija una opción válida dentro de la sección");
-            }
-        }
-        // ...
-        nombre = prompt("Si desea ingresar un nuevo producto ingreselo a continuación. Caso contrario escriba 'esc' para salir"); //Debe ingresar una categoría de bebida, caso contrario escriba 'ESC' para continuar
-    }
-    let envio = prompt("Desea elegir nuestro sistema de envio a domicilio?. A continuación seleccione SI o NO");
-    if ((envio === "si") || (envio === "SI")) {
-        alert(`Confirme sus datos ingresados: 
-        Nombre: ${persona1.nombre}
-        Edad: ${persona1.edad}
-        Dirección: ${persona1.direccion}`);
-        let confirmacion = prompt("Si son correctos escriba SI, caso contrario escriba MODIFICAR/NO");
-        switch (confirmacion) {
-            case "si":
-                alert("Excelente, su pedido sera preparado y enviado por nuestro cadete");
-                break;
-            case "SI":
-                alert("Excelente, su pedido sera preparado y enviado por nuestro cadete");
-                break;
-            default:
-                let nombre = prompt("Ingrese nuevamente su nombre");
-                let edad = parseInt(prompt("Ingrese nuevamente su edad"));
-                let direccion = prompt("Ingrese nuevamente su direccion de residencia");
-                const persona3 = new Persona(nombre, edad, direccion);
-                while (nombre != "ESC") {
-                    alert(`Confirme sus datos:
-                    Nombre: ${persona3.nombre}
-                    Edad: ${persona3.edad}
-                    Direccion: ${persona3.direccion}`);
-                    nombre = prompt("Debe volver a ingresar sus datos, caso contrario escriba 'ESC' para continuar")
-                }//ver como agregar informacion para el envio
-                alert("Excelente, su pedido sera preparado y enviado por nuestro cadete");
-                break;
-        }
-    }
-    else {
-        alert("Excelente, su pedido sera preparado para su posterior retiro por la sucursal. Recuerde que nuestro horario de atención es de Lunes a Viernes de 9:30 hs a 22 hs y días Sabados de 9:30 hs a 14 hs.")
-    }
-    alert("MUCHAS GRACIAS POR ELEGIRNOS...");
-}
-else {
-    alert("Debe completar con los datos requeridos");
-} */
-//-------------------------------------------------------------------------------------
 
 const agregar = (nombre) => {
     alert(`El producto ${nombre} fue agregado al carrito`);
